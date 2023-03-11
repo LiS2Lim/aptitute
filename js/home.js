d8ab27eeb4a118ad4d0c418da2cfc7dc4f54e35d
s2alua <s2alua.fu@gmail.com>
2023-03-11 02:46:57 +0000

    fixed: change home to morden design

--- js/home.js
+++ js/home.js
@@ -2,10 +2,19 @@ const startButtonEl = document.querySelector('#start');
 const inputNameEl = document.querySelector('#name');
 const dialog = document.querySelector('dialog');
 
-startButtonEl.addEventListener('click', () => {
+const GotoQuestion = () => {
 	if(inputNameEl.value === "") {
 		dialog.showModal();
 	} else {
 		location.href = '/questions/?name='+inputNameEl.value;
+	}
+}
+startButtonEl.addEventListener('click', () => {
+	GotoQuestion();
+})
+
+inputNameEl.addEventListener('keyup', () => {
+	if(window.event.keyCode === 13) {
+		GotoQuestion();
 	}
 })
